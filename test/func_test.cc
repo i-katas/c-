d9adc598afc8e776f10080ce5075da32f9ad43da
i-katas <feexonj@gmail.com>
2020-03-15 20:52:27 +0800

    pass by value v.s pass by reference

--- test/func_test.cc
+++ test/func_test.cc
@@ -7,3 +7,31 @@ bool function(void) {
 TEST(Function, returns_zero_if_statement_is_missing) {
   ASSERT_EQ(0, function());
 }
+
+
+//make parameters immutable through pass by value
+int triple(int n) {
+  n *= 3;
+  return n;
+}
+
+TEST(Function, pass_by_value) {
+  int n = 2;
+
+  ASSERT_EQ(6, triple(n));
+  ASSERT_EQ(2, n);
+}
+
+
+//make parameters mutable through pass by reference
+int tripled(int& n) {
+  n *= 3;
+  return n;
+}
+
+TEST(Function, pass_by_reference) {
+  int n = 2;
+
+  ASSERT_EQ(6, tripled(n));
+  ASSERT_EQ(6, n);
+}
