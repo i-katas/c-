0a6c503ee71fc80d072d1e2697b8c9e10b7c908c
i-katas <feexonj@gmail.com>
2020-03-24 01:12:28 +0800

    c style string with null terminator

--- test/string_test.cc
+++ test/string_test.cc
@@ -4,4 +4,14 @@ TEST(String, length) {
     std::string s{"string"};
 
     ASSERT_EQ(6u, s.length());
+
+}
+
+
+TEST(String, c_style_string_with_null_terminator) {
+    char s[]{"string"};
+
+    ASSERT_EQ(7u, std::size(s));
+    ASSERT_EQ('s', s[0]);
+    ASSERT_EQ('\0', s[6]) << "null terminator";
 }
