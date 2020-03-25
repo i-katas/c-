6f50ad1d7bb181660389c54ec7c159ccb7965891
i-katas <feexonj@gmail.com>
2020-03-26 04:03:43 +0800

    test compile & runtime assert

---
+++ test/assert_test.cc
@@ -0,0 +1,10 @@
+#include "gtest/gtest.h"
+
+TEST(Assert, assert_abort_in_runtime) {
+    //assert(1 == 2); 
+}
+
+
+TEST(Assert, static_assert_abort_in_compile_time) {
+    //static_assert(1 == 2);
+}
